4a9abac027139eaeb6e76ae94cb1fc36e5ce4302
bezawitassaye <beza1221love@gmail.com>
2025-10-13 00:23:54 +0300

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import GlobalProvider from "@/lib/global-provider";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -22,5 +23,12 @@ export default function RootLayout() {
   if (!fontsLoaded) {
     return null;
   }
-  return <Stack screenOptions={{headerShown:false}}/>;
+  return( 
+    <GlobalProvider>
+      <Stack screenOptions={{headerShown:false}}/>
+
+    </GlobalProvider>
+  
+
+);
 }
